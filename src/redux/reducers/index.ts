fceb3d330a6eb04948074aafbf8d7df2b8ebc10e
JyojiTenguam <j.y.oji@hotmail.com>
2024-04-10 15:52:12 -0300

    Resolvendo requisito 02 de exercise forms redux

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,1 +1,23 @@
-export default () => {}; // deletar esta linha
+import { AnyAction } from "redux";
+
+const INITIAL_STATE = {
+  personalData: {
+    name: '',
+    email: '',
+    cpf: '',
+    address: '',
+    city: '',
+    uf: '',
+  },
+  professionalData: {
+    resume: '',
+    role: '',
+    description: '',
+  },
+};
+
+const rootReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  return state;
+};
+
+export default rootReducer;
